f50e805e17c9a1e2cb749e1e8a915f7e4f463dc8
aZoG18 <65915381+aZoG18@users.noreply.github.com>
2020-05-25 19:15:53 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "botdiscord",
+  "version": "1.0.0",
+  "description": "",
+  "main": "main.js",
+  "dependencies": {
+    "discord.js": "^12.2.0",
+    "moment": "^2.26.0"
+  },
+  "devDependencies": {},
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}
